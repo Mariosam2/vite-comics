c3e90ac99380a11f4df067d7104549e2a8e659a3
Mariosam2 <marco.mariosaa@gmail.com>
2022-11-11 17:02:42 +0100

    edited name of Comic to ComicItem

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,12 +1,12 @@
 <script>
 import IconsComp from './IconsComp.vue';
 import Jumbotron from './Jumbotron.vue';
-import Comic from './ComicItem.vue';
+import ComicItem from './ComicItem.vue';
 import data from '../assets/data/data.js';
 export default {
     name: 'SiteMain',
     components: {
-        IconsComp, Jumbotron, Comic
+        IconsComp, Jumbotron, ComicItem
     },
     data() {
         return {
@@ -22,8 +22,8 @@ export default {
         <section class="content">
             <div class="container">
                 <div class="row row-cols-1 row-cols-md-3 row-cols-xl-6 g-3">
-                    <comic v-for="comic in comics" :img="comic.thumb" :title="comic.series">
-                    </comic>
+                    <comic-item v-for="comic in comics" :img="comic.thumb" :title="comic.series">
+                    </comic-item>
                 </div>
                 <div class="callToAction text-center">
                     <a href="#" class="btn btn-primary rounded-0 text-uppercase p-2 fw-bold">Load More</a>
